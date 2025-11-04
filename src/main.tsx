1dbe6c8d53eec129adab5a2c84db171b4222abcb
p0377758-source <p0377758@gmail.com>
2025-11-04 08:16:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initializeProductStore } from "./lib/productStore.ts"; // <-- LINHA ADICIONADA
+
+initializeProductStore(); // <-- LINHA ADICIONADA
 
 createRoot(document.getElementById("root")!).render(<App />);
